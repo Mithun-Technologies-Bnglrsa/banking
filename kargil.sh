2430b658454a1555bbff49c45d38979bbe098bba
venkateshpasupuleti <venkateshpasupuleti04@gmail.com>
2020-01-07 17:42:46 +0530

    supraja

--- kargil.sh
+++ kargil.sh
@@ -2,19 +2,4 @@
 Kargil is a place located in indo-pak border
 Kargil is a war between india and pakistan
 Kargil is a place of uncertainity
-aaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbb
-
-ccccccccccc
-cccccccccccccccccccc
-dddddddddddddddddddd
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-
-
-
-cccccccccccccc
-ddddddd
-50b4438d71dcf0e98e0b7b0a0629f458cfebfaed
-
-
-hemanth is very good boy.
+suprajaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
